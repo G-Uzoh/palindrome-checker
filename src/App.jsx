676598ffa16a93f3b77113be24d904136725f21a
George Uzoh <ugochukwuuzoh@gmail.com>
2023-11-11 09:50:59 +0200

    update palindrome function

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ function App() {
   const [input, setInput] = useState('');
   const [isPalindrome, setIsPalindrome] = useState(false);
   
+  // Global variable
+  const message = document.querySelector('#message');
+
   function checkPalindrome() {
-    const reversedString = input.split('').reverse().join('');setIsPalindrome(input === reversedString);
+    const reversedString = input.toLowerCase().split('').reverse().join('');
+    setIsPalindrome(input.toLowerCase() === reversedString);
+    
+    message.classList.remove('hide');
   }
+
+  const clear = () => hideMessageArea();
+
+  const hideMessageArea = () => message.classList.add('hide');
 
   return (
     <>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)} placeholder='Enter word or number' />
+      <h1>Palindrome Checker</h1>
+      <input type="text" value={input} onFocus={clear} onChange={e => setInput(e.target.value)} placeholder='Enter word or number' />
       <button onClick={checkPalindrome}>Submit</button>
-      {isPalindrome ? <p>True</p> : <p>False</p>}
+      <p id="message" className='hide'>{isPalindrome ? `${input} is a palindrome` : `${input} is not a palindrome`}</p>
     </>
   )
 }
